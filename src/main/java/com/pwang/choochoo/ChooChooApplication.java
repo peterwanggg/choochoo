f32ad22316219724df2cd6911ece8610da0453ee
Peter Wang <pwang@palantir.com>
2017-12-25 17:16:12 -0800

    rename

---
+++ src/main/java/com/pwang/choochoo/ChooChooApplication.java
@@ -0,0 +1,11 @@
+package com.pwang.choochoo;
+
+/**
+ * @author pwang on 12/25/17.
+ */
+public class ChooChooApplication {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
